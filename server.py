a5b253a8a9e678ed99e304ad08e8f1c6dd9b024f
Glitch (what-should-i-wear) <none>
2020-09-13 14:32:39 +0000

    Final

--- server.py
+++ server.py
@@ -46,3 +46,46 @@ def save_location():
 
 if __name__ == "__main__":
   app.run()
+
+# API JSON Format
+# {
+# 	u'current': {
+# 		u'gust_kph': 15.8, 
+# 		u'last_updated': u'2020-09-12 22:15', 
+# 		u'vis_miles': 9.0, 
+# 		u'pressure_in': 30.7, 
+# 		u'cloud': 0, 
+# 		u'precip_mm': 0.0, 
+# 		u'is_day': 0, 
+# 		u'feelslike_c': 20.0, 
+# 		u'condition': {
+# 			u'text': u'Clear', 
+# 			u'code': 1000, 
+# 			u'icon': u'//cdn.weatherapi.com/weather/64x64/night/113.png'
+# 		}, 
+# 		u'feelslike_f': 68.0, 
+# 		u'wind_mph': 8.1, 
+# 		u'temp_f': 68.0, 
+# 		u'temp_c': 20.0, 
+# 		u'last_updated_epoch': 1599963306, 
+# 		u'pressure_mb': 1024.0, 
+# 		u'vis_km': 16.0, 
+# 		u'precip_in': 0.0, 
+# 		u'wind_dir': u'E', 
+# 		u'wind_kph': 13.0, 
+# 		u'uv': 1.0, 
+# 		u'humidity': 73, 
+# 		u'gust_mph': 9.8, 
+# 		u'wind_degree': 100
+# 	}, 
+# 	u'location': {
+# 		u'name': u'Foxhall', 
+# 		u'country': u'United States of America', 
+# 		u'region': u'Virginia', 
+# 		u'tz_id': u'America/New_York', 
+# 		u'lon': -77.18, 
+# 		u'lat': 38.91, 
+# 		u'localtime_epoch': 1599963346, 
+# 		u'localtime': u'2020-09-12 22:15'
+# 	}
+# }
